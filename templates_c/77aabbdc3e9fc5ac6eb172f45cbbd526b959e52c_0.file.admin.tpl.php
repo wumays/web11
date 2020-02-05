dbd53a4b69027dbd7f10bb2976b7e12e35c78310
wumays <wumays720@gmail.com>
2020-02-05 13:32:15 +0800

    first commit

---
+++ templates_c/77aabbdc3e9fc5ac6eb172f45cbbd526b959e52c_0.file.admin.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-02-05 04:59:21
+  from 'D:\ugm\xampp\htdocs\web11\templates\tpl\admin.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5e3a3d999330b2_51389468',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '77aabbdc3e9fc5ac6eb172f45cbbd526b959e52c' => 
+    array (
+      0 => 'D:\\ugm\\xampp\\htdocs\\web11\\templates\\tpl\\admin.tpl',
+      1 => 1580875133,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e3a3d999330b2_51389468 (Smarty_Internal_Template $_smarty_tpl) {
+?><h1>育將電腦工作室 後台</h1><?php }
+}
